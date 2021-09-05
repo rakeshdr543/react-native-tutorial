4a1e8a2a1e2a1e00d68f4627193976c9a802dd8f
Rakesh D R <rakeshdr543@gmail.com>
2021-09-05 21:19:32 +0530

    Added flatlist

--- App.js
+++ App.js
@@ -1,40 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import {Button, ScrollView, StyleSheet, Text, TextInput, View} from 'react-native';
+import {Button, FlatList, ScrollView, StyleSheet, Text, TextInput, View} from 'react-native';
 
 export default function App() {
- const [people,setPeople]=useState([
-   {name:'ram',age:15},
-   {name:'ran',age:16},
-   {name:'rak',age:17},
-   {name:'rav',age:18},
-     {name:'ram',age:15},
-     {name:'ran',age:16},
-     {name:'rak',age:17},
-     {name:'rav',age:18},
-     {name:'ram',age:15},
-     {name:'ran',age:16},
-     {name:'rak',age:17},
-     {name:'rav',age:18},
+  const DATA = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'First Item',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Second Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Third Item',
+    },
+  ];
 
- ])
-
+  const Item = ({ title }) => (
+      <View style={styles.item}>
+        <Text style={styles.title}>{title}</Text>
+      </View>
+  );
+  const renderItem = ({ item }) => (
+      <Item title={item.title} />
+  );
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text>Name & Age</Text>
       </View>
-        <ScrollView>
-      {
-        people.map((item)=>{
-         return(
-             <View key={item.age} style={styles.body}>
-               <Text>{item.name} present age is {item.age}!!!</Text>
-             </View>
-         )
-        })
-      }
-        </ScrollView>
+      <FlatList
+          data={DATA}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+      />
     </View>
   );
 }
